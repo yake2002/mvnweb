ee939655891e17fbd49b6ca7130b8b50ae5b917a
unknown <yake2002@sina.com>
2018-02-01 13:10:22 +0800

    ll

--- src/main/java/com/bbs/web/LoginController.java
+++ src/main/java/com/bbs/web/LoginController.java
@@ -1,9 +1,12 @@
 package com.bbs.web;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bbs.domain.User;
 import com.bbs.service.UserService;
 
 @Controller
@@ -16,6 +19,22 @@ public class LoginController {
 	public String toLoginPage() {
 
 		return "login";
+
+	}
+	
+	@RequestMapping(value = "/checkLogin")
+	public String loginPage(HttpServletRequest request, User user) {
+
+		Boolean loginSucess = userService.hasMatchUser(user);
+
+		if (loginSucess) {
+			userService.loginSucess(userService.findUserByUserName(user));
+			request.getSession().setAttribute("Info", "");
+			return "welcome";
+		} else {
+			request.getSession().setAttribute("Info", "请重新输入!!");
+			return "redirect:/login.do";
+		}
 
 	}
 
--- src/main/webapp/WEB-INF/views/welcome.jsp
+++ src/main/webapp/WEB-INF/views/welcome.jsp
@@ -1,8 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-
 <html>
 
 <head>
@@ -11,12 +9,8 @@
 </head>
 
 <body>
-	${Info} 请输入用户名,密码登陆:
-	<form action="/mvnweb/checkLogin.do" meathod="post">
-		用户名: <input type="text" name="userName"> <br> 密码: <input
-			type="password" name="password"> <br> <input
-			type="submit" value="登陆"> <input type="reset" value="重置">
-	</form>
+	登陆成功!
+	<br> 欢迎光临!
 </body>
 
 </html>
